35c38466a43164adea719d19ba86804ef1f58a28
Priyanshu Maurya <priyanshu1999pm@gmail.com>
2023-08-03 10:45:53 +0000

    fronted complted
    completed frontend and tiny bit of changes in metadeta to cutomize the application also made customized readme.md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import { Select, Space, Input } from 'antd';
+import React, { useState } from 'react';
+import { Select, Space, Input, Button } from 'antd';
 import { useCharacterContext } from "../store/filter-context";
-
+import { CaretLeftOutlined } from '@ant-design/icons';
+import { useNavigate } from "react-router-dom";
 
 const { Search } = Input;
 
 
 
-export default function Header() {
+export default function Header(prop) {
 
     const { name, status, gender, setName, setStatus, setGender } =
         useCharacterContext();
+    const navigate = useNavigate();
+    const [size, setSize] = useState('large');
 
     return (
-        <div>
-            <Space direction='vertical'>
-                <Search placeholder="enter you fav character" style={{
+        <header className='App-header' style={{}}
+        >
+
+            <Button style={{ marginLeft: '0px', position: 'relative', visibility: prop.goback, left: '30px' }} type="primary" shape="circle" onClick={() => navigate(-1)} icon={<CaretLeftOutlined />} size={size} />
+
+            <h2 style={{ position: 'relative', right: '15vh' }}>Rick and Morty App</h2>
+
+            <Space style={{
+                visibility: prop.filter,
+            }}>
+                <Search placeholder="Who is your favorite character?" style={{
                     width: 250,
                 }} onSearch={(value) => setName(value)} enterButton />
 
@@ -43,7 +54,7 @@ export default function Header() {
                 />
             </Space>
 
-        </div>
+        </header>
     )
 }
 
---
+++ src/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { CharacterProvider } from "./store/filter-context";
+import CharacterDetail from "./pages/CharacterDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from 'antd';
+import Header from './components/Header';
+
+let year = new Date().getFullYear();
+
+const ApplyLayout = (page, pagename) => <Layout>
+  <Header filter={pagename == 'app' ? '' : 'hidden'} goback={pagename == 'app' ? 'hidden' : ''} />
+
+  {page}
+  <footer style={{ textAlign: 'center', position: 'sticky', top: '95vh', fontSize: '16px', marginTop: "30px", marginBottom: "20px" }}>
+
+    <span>
+      ©RickandMortyCharacters {year}{" "}. All Rights Reserved.
+    </span>
+  </footer>
+</Layout>;
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CharacterProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ApplyLayout(<App />, 'app')} />
+          <Route path="/:id" element={ApplyLayout(<CharacterDetail />, 'character-detail')} />
+        </Routes>
+      </BrowserRouter>
+    </CharacterProvider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
